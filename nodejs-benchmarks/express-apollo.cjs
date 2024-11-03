cdbd37de9623a47e74091b4a205b1a813494181a
Tomek Nieżurawski <tommaqs@gmail.com>
2024-11-03 16:27:27 +0100

    Add nodejs-express-apollo

---
+++ nodejs-benchmarks/express-apollo.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+const cors = require('cors');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const express = require('express');
+const { createApolloSchema } = require('../lib/schemas/createApolloSchema.cjs');
+
+const app = express();
+const schema = createApolloSchema();
+const server = new ApolloServer({
+  schema,
+});
+server.start().then(() => {
+  app.use('/graphql', cors(), express.json(), expressMiddleware(server, {}));
+  app.listen(3000);
+});
