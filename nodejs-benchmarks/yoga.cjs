52412e23ad74dd0b06c802ef0554afb28a65d665
Tomek Nieżurawski <tommaqs@gmail.com>
2024-11-05 19:05:09 +0100

    Add yoga (standalone)

---
+++ nodejs-benchmarks/yoga.cjs
@@ -0,0 +1,15 @@
+'use strict';
+
+const { createServer } = require('node:http');
+const { createYoga } = require('graphql-yoga');
+const { createApolloSchema } = require('../lib/schemas/createApolloSchema.cjs');
+
+const schema = createApolloSchema();
+const yoga = createYoga({
+  graphqlEndpoint: '/graphql',
+  schema,
+});
+
+const server = createServer(yoga);
+
+server.listen(3000);
